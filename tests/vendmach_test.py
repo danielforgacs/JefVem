51eb97f474abf8e089977861d9b89729be329e34
df@ <@>
2021-04-26 20:52:02 +0100

    clean up; refactor

--- tests/vendmach_test.py
+++ tests/vendmach_test.py
@@ -11,11 +11,11 @@ def test_can_init_vendingmachine_w_item_and_coins():
 
 def test_vendingmachine_knows_if_coins_are_enough():
     choice = list(vendmach.VALID_ITEMS)[0]
-    item = vendmach.VendingMachine(itemrequest=choice, coins=[10])
-    assert not item.is_payed
+    machine = vendmach.VendingMachine(itemrequest=choice, coins=[10])
+    assert not machine.is_payed
 
-    item.coins = [50, 50, 50, 50]
-    assert item.is_payed
+    machine.coins = [50, 50, 50, 50]
+    assert machine.is_payed
 
 
 
