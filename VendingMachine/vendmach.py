6a0daedbc0428a271137eb056816ff7833a39fc2
df@ <@>
2021-04-26 17:23:53 +0100

    wrote test; udpated vending machine

--- VendingMachine/vendmach.py
+++ VendingMachine/vendmach.py
@@ -1,8 +1,26 @@
-options = {
+VALID_ITEMS = {
     'nuke license': 200,
     'renderfarm time': 10000,
 }
+VALID_COINS = [10, 20, 50]
 
 
 class VendingMachine:
     def __init__(self, item, coins):
+        if not item in VALID_ITEMS:
+            raise Exception('[ERROR] Invalid item.')
+
+        is_goodcion = lambda x: x in VALID_COINS
+
+        if not all(map(is_goodcion, coins)):
+            raise Exception('[ERROR] Valid coin values: {}'.format(VALID_COINS))
+
+        self.item = item
+        self.coins = coins
+
+
+
+if __name__ == '__main__':
+    pass
+
+    result = VendingMachine(item='nuke license', coins=[1])
--- tests/vendmach_test.py
+++ tests/vendmach_test.py
@@ -1,12 +1,7 @@
-# import VendingMachine.vendmach as vendmach
-#
-#
-#
-# def test_can_init_vendingmachine_w_item_and_coins():
-#     response = vendmach.VendingMachine(item=1, coins=1)
-#
-#     assert 0
+import VendingMachine.vendmach as vendmach
 
 
-def test_hjg():
-    assert False
+
+def test_can_init_vendingmachine_w_item_and_coins():
+    choice = list(vendmach.VALID_ITEMS)[0]
+    response = vendmach.VendingMachine(item=choice, coins=[10])
