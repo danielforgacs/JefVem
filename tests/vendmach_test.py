a1a8d3179948f8915c3e6981b3902a3f870231df
df@ <@>
2021-04-26 20:23:59 +0100

    updated tests

--- tests/vendmach_test.py
+++ tests/vendmach_test.py
@@ -49,4 +49,12 @@ def test_vending_machine_errors_on_bad_arguments():
 def test_vendingmachine_returns_change_if_overpaif():
     machine = vendmach.VendingMachine(itemrequest='nuke license', coins=[10, 10, 20])
     assert machine.item == 'nuke license'
-    assert machine.change == [10]
+    assert sorted(machine.change) == sorted([10])
+
+    machine = vendmach.VendingMachine(itemrequest='nuke license', coins=[20, 20])
+    assert machine.item == 'nuke license'
+    assert sorted(machine.change) == sorted([10])
+
+    machine = vendmach.VendingMachine(itemrequest='nuke license', coins=[20, 20, 20])
+    assert machine.item == 'nuke license'
+    assert sorted(machine.change) == sorted([20, 10])
