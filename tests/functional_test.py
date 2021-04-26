86cf767e812d7b5f7acdd2c10f5ceb1d39e08a96
df@ <@>
2021-04-26 20:05:57 +0100

    wrote tests

--- tests/functional_test.py
+++ tests/functional_test.py
@@ -3,7 +3,13 @@ import VendingMachine.vendmach as vendmach
 
 
 def test_functionality_example():
+    itemname = 'renderfarm time'
+    budget = []
     item = vendmach.VendingMachine(
-        itemrequest='renderfarm time',
-        coins=[10]
+        itemrequest=itemname,
+        coins=budget,
     )
+
+    assert item.item is None
+
+    print('item cost:', item.cost)
